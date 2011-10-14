ecffb683c298b84c548942ef58c3caf3b968fb40
Mariusz Okrój <okrojmariusz@gmail.com>
2011-10-14 20:10:01 +0200

    Poprawiony bug w funkcji normalize()

--- Vector3d.h
+++ Vector3d.h
@@ -45,7 +45,8 @@ public:
                 return sqrt (d[0] * d[0] + d[1] * d[1] + d[2] * d[2]);
         }
         void normalize () {
-                foreachcoord_i d[i] /= length ();
+                float old_length = length();
+                foreachcoord_i d[i] /= old_length;
         }
         void print () {
                 printf ("v %f %f %f\n", d[0], d[1], d[2]);
